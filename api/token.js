897f160b3fa5bb55bc2cc273ffdc2f56f79c5ef0
maruuya01 <reusorarobert@gmail.com>
2025-07-19 10:46:53 +0800

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,10 +1,7 @@
-// /pages/api/token.js
-
 export default async function handler(req, res) {
   const token = Math.random().toString(36).substring(2, 10);
-  const expires = Date.now() + 1000 * 60 * 5;
 
-  await fetch(`https://teaching-mongoose-18450.upstash.io/set/${token}/${expires}`, {
+  await fetch(`https://teaching-mongoose-18450.upstash.io/set/${token}/valid`, {
     headers: {
       Authorization: 'Bearer AUgSAAIjcDE3MGE5NjEwY2NiZmE0YTZmYWY2ZjNhODJmNDI5ODliOXAxMA',
     },
